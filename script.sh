3883ed9c6af7e1385c6e22cd6fe11f1378520027
İbrahim Hakkı Ergin <ibrahimh.ergin@gmail.com>
2025-07-18 15:19:56 +0300

    Refactor uptime script to accept website parameter and improve output messages

--- script.sh
+++ script.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
+echo "Welcome to uptime controller"
+WEBSITE=$1
+if [ "$WEBSITE" != "" ]; then
+    echo "$WEBSITE"
 
-site="https://kosakymm.com"
-echo "Kontrol ediliyor: $site"
+    while :
+    do
+        curl -s --head -o /dev/null "$WEBSITE"
+        status=$?
 
-while :
-do
-    curl -s --head -o /dev/null "$site"
-    status=$?
-
-    if [ "$status" -eq 0 ]; then
-        echo "Sonuç: ONLINE (Çıkış Kodu: $status)"
-    else
-        echo "Sonuç: OFFLINE (Çıkış Kodu: $status)"
-    fi
-done
+        if [ "$status" -eq 0 ]; then
+            echo "ONLINE ($status)"
+        else
+            echo "OFFLINE ($status)"
+        fi
+    done
+else 
+    echo -e "\e[31mYou must enter a parameter as a website \e[0m "
+fi
