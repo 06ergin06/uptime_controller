b622f9f77fce4efb614c38f12290ff2715dfc838
İbrahim Hakkı Ergin <ibrahimh.ergin@gmail.com>
2025-07-18 15:28:10 +0300

    Enhance uptime script with online/offline counters and reporting function

--- script.sh
+++ script.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 echo "Welcome to uptime controller"
+
+online=0
+offline=0
+
+
 WEBSITE=$1
+function report(){
+    echo "-------- Uptime Report $WEBSITE : --------"
+    echo -e "\e[32mOnline count : $online \e[0m"
+    echo -e "\e[31mOffline count : $offline \e[0m"
+    exit 0
+}
 if [ "$WEBSITE" != "" ]; then
     echo "$WEBSITE"
 
@@ -11,9 +22,12 @@ if [ "$WEBSITE" != "" ]; then
 
         if [ "$status" -eq 0 ]; then
             echo "ONLINE ($status)"
+            online=$((online + 1))
         else
             echo "OFFLINE ($status)"
+            offline=$((offline + 1))
         fi
+        trap report INT
     done
 else 
     echo -e "\e[31mYou must enter a parameter as a website \e[0m "
